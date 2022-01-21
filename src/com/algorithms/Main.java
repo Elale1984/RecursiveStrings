3711c12215aaaff5ffa5dd65dab0c0d7842f025a
Elale1984 <69740507+Elale1984@users.noreply.github.com>
2022-01-21 16:22:46 -0600

    removed unnecessary import

--- src/com/algorithms/Main.java
+++ src/com/algorithms/Main.java
@@ -2,7 +2,6 @@ package com.algorithms;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
