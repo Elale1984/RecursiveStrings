a644a3e752ba8af2519f18a70eaa13a796d4884c
Elale1984 <69740507+Elale1984@users.noreply.github.com>
2022-01-22 00:32:37 -0600

    slip finished, working on slap

--- src/com/algorithms/Main.java
+++ src/com/algorithms/Main.java
@@ -10,58 +10,79 @@ public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
         Scanner readIn = new Scanner(new File("Strings.txt"));
-        List<String> slops = new ArrayList<>();
-        List<String> slaps = new ArrayList<>();
-        List<String> slips = new ArrayList<>();
+        List<Boolean> slops = new ArrayList<>();
+        List<Boolean> slaps = new ArrayList<>();
+        List<Boolean> slips = new ArrayList<>();
 
         while (readIn.hasNext()){
             String word = readIn.next();
-            if(isSlop(word))
-                slops.add("Yes");
-            else
-                slops.add("No");
-            if(isSlap(word))
-                slaps.add("Yes");
-            else
-                slaps.add("No");
             if(isSlip(word))
-                slips.add("Yes");
+                slips.add(true);
             else
-                slips.add("No");
+                slips.add(false);
+            if(isSlap(word))
+                slaps.add(true);
+            else
+                slaps.add(false);
+//            if(isSlip(word))
+//                slips.add("Yes");
+//            else
+//                slips.add("No");
         }
 
-        System.out.println("Slops Output:");
-        printOutput(slops);
+        System.out.println("Slips Output:");
+        printOutput(slips);
         System.out.println("End of Output\n\n");
         System.out.println("Slaps Output:");
         printOutput(slaps);
         System.out.println("End of Output\n\n");
-        System.out.println("Slips Output:");
-        printOutput(slips);
-        System.out.println("End of Output\n\n");
+//        System.out.println("Slips Output:");
+//        printOutput(slips);
+//        System.out.println("End of Output\n\n");
 
     }
 
-    private static void printOutput(List<String> list) {
-        for (String answer:
+    private static void printOutput(List<Boolean> list) {
+        for (var answer:
                 list) {
-            System.out.println(answer);
+            if(answer)
+                System.out.println("Yes");
+            else
+                System.out.println("No");
+
         }
     }
 
-    private static boolean isSlop(String word) {
-        boolean answer = false;
-        return answer;
-    }
+//    private static boolean isSlop(String word) {
+//
+//    }
 
     private static boolean isSlap(String word) {
-        boolean answer = false;
-        return answer;
+        if(word.length() == 0)
+            return false;
+        else if(word.length() == 2 && word.charAt(0) == 'A')
+            return true;
+        else if (word.charAt(0) == 'A' && word.length() > 2){
+            if (isSlip(word.substring(1, word.length() - 2)) && word.charAt(word.length() - 1) == 'C'){
+                return true;
+            }
+        }
     }
 
     private static boolean isSlip(String word) {
-        boolean answer = false;
-        return answer;
-
+        if(word.length() == 0)
+            return false;
+        else if(word.charAt(0) == 'G' && word.length() == 1){
+            return true;
+        }
+        else if (word.charAt(0) == 'F' || word.charAt(0) == 'D' || word.charAt(0) == 'E' ){
+            return isSlip(word.substring(1));
+        }
+        else{
+            if(word.charAt(0) != 'F' || word.charAt(0) != 'D' || word.charAt(0) != 'E' ){
+                return false;
+            }
+        }
+        return isSlip(word.substring(1));
     }
 }
