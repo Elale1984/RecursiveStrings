0b60563e771f9a125dcd429159dc33b093cd9a91
Elale1984 <69740507+Elale1984@users.noreply.github.com>
2022-01-23 02:10:34 -0600

    Code Cleanup

--- src/com/algorithms/Main.java
+++ src/com/algorithms/Main.java
@@ -10,24 +10,25 @@ public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
         Scanner readIn = new Scanner(new File("Strings.txt"));
-        List<Boolean> slops = new ArrayList<>();
-        List<Boolean> slaps = new ArrayList<>();
-        List<Boolean> slips = new ArrayList<>();
+        List<String> slops = new ArrayList<>();
+        List<String> slaps = new ArrayList<>();
+        List<String> slips = new ArrayList<>();
 
+        readIn.next();
         while (readIn.hasNext()){
             String word = readIn.next();
             if(isSlip(word))
-                slips.add(true);
+                slips.add("Yes");
             else
-                slips.add(false);
+                slips.add("No");
             if(isSlap(word))
-                slaps.add(true);
+                slaps.add("Yes");
             else
-                slaps.add(false);
+                slaps.add("No");
             if(isSlop(word))
-                slops.add(true);
+                slops.add("Yes");
             else
-                slops.add(false);
+                slops.add("No");
         }
 
         System.out.println("Slips Output:");
@@ -42,16 +43,30 @@ public class Main {
 
     }
 
-    private static void printOutput(List<Boolean> list) {
+    /**
+     * @param list
+     * Helper function to print out the lists of answers
+     */
+
+    private static void printOutput(List<String > list) {
         for (var answer:
                 list) {
-            if(answer)
-                System.out.println("Yes");
-            else
-                System.out.println("No");
+            System.out.println(answer);
 
         }
     }
+
+    /**
+     *
+     * @param word
+     * takes in a string that is read from the file.
+     * @return
+     * determines whether the string is a slop using recursion.
+     *
+     * Constraints:
+     * •Starts with a slap
+     * •Ends with a slip.
+     */
 
     private static boolean isSlop(String word) {
         int counter = 1;
@@ -66,6 +81,22 @@ public class Main {
         return false;
     }
 
+    /**
+     *
+     * @param word
+     * takes in a string that is read from the file.
+     * @return
+     * determines whether the string is a slap using recursion.
+     *
+     * Constraints:
+     * •Its first character is an 'A'
+     * •If it is a two-character Slap then its second and last character is an 'H'
+     * •If it is not a two-character Slap, then it is in one of these two forms:o'A'
+     *      followed by 'B' followed by a Slap, followed by a 'C'o'A' followed by a Slip (see above) followed by a 'C'
+     *  •Nothing else is a Slap
+     *
+     */
+
     private static boolean isSlap(String word) {
         if(word.length() < 2)
             return false;
@@ -73,12 +104,30 @@ public class Main {
             return true;
         else if (word.equals("ABC"))
             return true;
-        else if (word.charAt(0) == 'A' && word.charAt(1) == 'B' && word.charAt(word.length() - 1) == 'C'){
+        else if (word.charAt(0) == 'A' && word.charAt(1) == 'B' &&
+                word.charAt(word.length() - 1) == 'C'){
             return isSlap(word.substring(2, word.length() - 1));
         }
-        else return word.charAt(0) == 'A' && isSlip(word.substring(1, word.length() - 1)) && word.charAt(word.length() - 1) == 'C';
+        else return word.charAt(0) == 'A' && isSlip(word.substring(1, word.length() - 1)) &&
+                    word.charAt(word.length() - 1) == 'C';
     }
 
+    /**
+     *
+     * @param word
+     * takes in a string that is read from the file.
+     * @return
+     * determines whether the string is a slip using recursion.
+     *
+     * Constraints:
+     * Its first character is either a 'D' or an 'E'
+     * •The first character is followed by a string of one or more 'F's
+     * •The string of one or more 'F's is followed by either a Slip or a 'G'
+     * •The Slip or 'G' that follows the F's ends the Slip. For example,
+     *      DFFEFFFG is a Slip since it has a 'D' for its first character,
+     *      followed by a string of two F's, and ended by the Slip 'EFFFG'
+     * •Nothing else is a Slip
+     */
     private static boolean isSlip(String word) {
         if(word.length() == 0)
             return false;
