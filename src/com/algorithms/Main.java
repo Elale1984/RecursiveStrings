f6f8263f7014af9d104e402607bc1b4c42c165bc
Elale1984 <69740507+Elale1984@users.noreply.github.com>
2022-01-23 01:20:16 -0600

    finished slop

--- src/com/algorithms/Main.java
+++ src/com/algorithms/Main.java
@@ -24,10 +24,10 @@ public class Main {
                 slaps.add(true);
             else
                 slaps.add(false);
-//            if(isSlip(word))
-//                slips.add("Yes");
-//            else
-//                slips.add("No");
+            if(isSlop(word))
+                slops.add(true);
+            else
+                slops.add(false);
         }
 
         System.out.println("Slips Output:");
@@ -36,9 +36,9 @@ public class Main {
         System.out.println("Slaps Output:");
         printOutput(slaps);
         System.out.println("End of Output\n\n");
-//        System.out.println("Slips Output:");
-//        printOutput(slips);
-//        System.out.println("End of Output\n\n");
+        System.out.println("Slops Output:");
+        printOutput(slops);
+        System.out.println("End of Output\n\n");
 
     }
 
@@ -53,9 +53,18 @@ public class Main {
         }
     }
 
-//    private static boolean isSlop(String word) {
-//
-//    }
+    private static boolean isSlop(String word) {
+        int counter = 1;
+
+        while (counter < word.length()){
+            if(isSlap(word.substring(0,counter + 1)))
+                return isSlip(word.substring(counter + 1));
+            else {
+                counter++;
+            }
+        }
+        return false;
+    }
 
     private static boolean isSlap(String word) {
         if(word.length() < 2)
@@ -65,14 +74,9 @@ public class Main {
         else if (word.equals("ABC"))
             return true;
         else if (word.charAt(0) == 'A' && word.charAt(1) == 'B' && word.charAt(word.length() - 1) == 'C'){
-            if(isSlap(word.substring(2,word.length() - 1))){
-                return true;
-            }
+            return isSlap(word.substring(2, word.length() - 1));
         }
-        else if (word.charAt(0) == 'A' && isSlip(word.substring(1, word.length() - 1)) && word.charAt(word.length() - 1) == 'C'){
-           return true;
-        }
-        return false;
+        else return word.charAt(0) == 'A' && isSlip(word.substring(1, word.length() - 1)) && word.charAt(word.length() - 1) == 'C';
     }
 
     private static boolean isSlip(String word) {
